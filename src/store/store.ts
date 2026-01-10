9005e938a03406a22cdcbfa260314bdb088a2105
hashbukhtiar <hashbukhtiar@users.noreply.github.com>
2026-01-10 13:14:33 -0500

    added persist middleware to emulate 'db' via local storage for the time being and added type for workspace

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,17 @@
 import { AppNode } from "@/types/nodes";
+import { UIMessage } from "ai";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type WorkSpaceNode = {
+	node: AppNode;
+	messages: UIMessage; // placeholder for when i get
+};
+
+type MindMapWorkspace = {
+	title: string;
+	nodes: WorkSpaceNode[];
+};
 
 type MindMapActions = {
 	setSelectedNode: (node: AppNode | null) => void;
@@ -13,16 +25,28 @@ type MindMapStore = {
 	actions: MindMapActions;
 };
 
-export const useMindMapStore = create<MindMapStore>((set) => ({
-	selectedNode: null,
-	chatNodes: null,
-	isChatBarOpen: false,
-	actions: {
-		setSelectedNode(node: AppNode | null) {
-			set({ selectedNode: node });
-		},
-		setIsChatBarOpen() {
-			set((state) => ({ isChatBarOpen: !state.isChatBarOpen }));
-		},
-	},
-}));
+export const useMindMapStore = create<MindMapStore>()(
+	persist(
+		(set) => ({
+			selectedNode: null,
+			chatNodes: null,
+			isChatBarOpen: false,
+			actions: {
+				setSelectedNode(node: AppNode | null) {
+					set({ selectedNode: node });
+				},
+				setIsChatBarOpen() {
+					set((state) => ({ isChatBarOpen: !state.isChatBarOpen }));
+				},
+			},
+		}),
+		{
+			name: "mind-map-state",
+			partialize: (state) => ({
+				selectedNode: state.selectedNode,
+				isChatBarOpen: state.isChatBarOpen,
+				chatNodes: state.chatNodes,
+			}),
+		}
+	)
+);

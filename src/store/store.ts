e4b359c446f25d1b24de867f87ee4eeb2d8918cc
abdullah73k <abdullah73k@users.noreply.github.com>
2026-01-10 13:42:56 -0500

    transfered useNodeState to zustand

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,11 @@
 import { AppNode } from "@/types/nodes";
+import {
+	applyNodeChanges,
+	Connection,
+	Edge,
+	EdgeChange,
+	NodeChange,
+} from "@xyflow/react";
 import { UIMessage } from "ai";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
@@ -6,6 +13,7 @@ import { persist } from "zustand/middleware";
 type WorkSpaceNode = {
 	node: AppNode;
 	messages: UIMessage[]; // placeholder for when i get
+	edges: Edge[];
 };
 
 type MindMapWorkspace = {
@@ -18,6 +26,12 @@ type MindMapActions = {
 	setSelectedNode: (node: AppNode | null) => void;
 	setIsChatBarOpen: () => void;
 	createWorkspace: () => void;
+	deleteWorkspace: (id: string) => void;
+
+	setActiveWorkspace: (id: string) => void;
+	onNodesChangeForActive: (changes: NodeChange<AppNode>[]) => void;
+	// onEdgesChangeForActive: (changes: EdgeChange<Edge>[]) => void;
+	// onConnectForActive: (connection: Connection) => void;
 };
 
 type MindMapStore = {
@@ -25,14 +39,16 @@ type MindMapStore = {
 	isChatBarOpen: boolean;
 	actions: MindMapActions;
 	workspaces: MindMapWorkspace[];
+	activeWorkspaceId: string | null;
 };
 
 export const useMindMapStore = create<MindMapStore>()(
 	persist(
-		(set) => ({
+		(set, get) => ({
 			selectedNode: null,
 			isChatBarOpen: false,
 			workspaces: [],
+			activeWorkspaceId: null,
 			actions: {
 				setSelectedNode(node: AppNode | null) {
 					set({ selectedNode: node });
@@ -41,11 +57,12 @@ export const useMindMapStore = create<MindMapStore>()(
 					set((state) => ({ isChatBarOpen: !state.isChatBarOpen }));
 				},
 				createWorkspace() {
+					const newWorkspaceId = crypto.randomUUID();
 					set((state) => ({
 						workspaces: [
 							...state.workspaces,
 							{
-								id: crypto.randomUUID(),
+								id: newWorkspaceId,
 								title: "New Workspace",
 								nodes: [
 									{
@@ -56,16 +73,61 @@ export const useMindMapStore = create<MindMapStore>()(
 											data: { title: "Main Topic of This Mindspace" },
 										},
 										messages: [],
+										edges: [],
 									},
 								],
 							},
 						],
+						activeWorkspaceId: newWorkspaceId,
 					}));
 				},
 				deleteWorkspace(id: string) {
 					set((state) => ({
-						workspaces: state.workspaces.filter((workspace) => workspace.id !== id),
+						workspaces: state.workspaces.filter(
+							(workspace) => workspace.id !== id
+						),
+						activeWorkspaceId: null,
 					}));
+				},
+				setActiveWorkspace(id: string) {
+					set((state) => ({
+						activeWorkspaceId: id,
+					}));
+				},
+				onNodesChangeForActive(changes) {
+					const state = get();
+					if (state.workspaces.length === 0) return;
+
+					const activeWorkspace = state.workspaces.find(
+						(workspace) => workspace.id === state.activeWorkspaceId
+					);
+
+					if (!activeWorkspace) return;
+
+					const nodesSnapshot = activeWorkspace.nodes.map(
+						(node) => node.node
+					) as AppNode[];
+
+					const updatedNodes = applyNodeChanges(changes, nodesSnapshot);
+
+					const updatedWorkSpaceNodes = activeWorkspace.nodes.map((wsNode) => {
+						const newNode = updatedNodes.find(
+							(node) => node.id === wsNode.node.id
+						);
+						return newNode
+							? {
+									...wsNode,
+									node: newNode,
+							  }
+							: wsNode;
+					});
+					const updatedWorkspace = {
+						...activeWorkspace,
+						nodes: updatedWorkSpaceNodes,
+					};
+					set({
+						workspaces: [updatedWorkspace, ...state.workspaces],
+					});
 				},
 			},
 		}),
@@ -75,6 +137,7 @@ export const useMindMapStore = create<MindMapStore>()(
 				selectedNode: state.selectedNode,
 				isChatBarOpen: state.isChatBarOpen,
 				workspaces: state.workspaces,
+				activeWorkspaceId: state.activeWorkspaceId,
 			}),
 		}
 	)
